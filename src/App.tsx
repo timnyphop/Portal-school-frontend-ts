0c0eca4eb9fbd5df07339b73cdc71f2d2add37ea
tinpo <137786541+timnyphop@users.noreply.github.com>
2024-06-12 20:40:02 +0700

    delete imports in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import { FC } from "react";
-import  {Cards}  from "./components/Cards/Cards";
 import React from "react";
-import data from './data/data.json';
-import { Ischool } from "./types/types";
-import {Routes, Route, Outlet, RouterProvider } from "react-router-dom";
-import School from "./pages/school/[id]/page.js";
-import { Header } from './components/Header/Header';
-import { Footer } from './components/Footer/Footer';
-import { Banner } from "./components/Banner/Banner";
-import { Promo } from "./components/Promo/Promo";
-import {Layout} from './pages/Layout/Layout';
+import {RouterProvider } from "react-router-dom";
+
 import { routes } from "./routes/routes";
-const data2:Ischool[]=data as Ischool[];
+
 export const App:FC=()=>{
   
   return(
